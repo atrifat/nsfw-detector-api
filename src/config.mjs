6f96051bc5f3204c98a2e3b4d9346787d9d31688
Rif'at Ahdi R <10791791+atrifat@users.noreply.github.com>
2025-07-10 09:50:17 +0800

    fix(config): incorrect env target for ENABLE_CONTENT_TYPE_CHECK

--- src/config.mjs
+++ src/config.mjs
@@ -22,7 +22,7 @@ export const config = {
     : false,
   API_TOKEN: process.env.API_TOKEN || 'myapitokenchangethislater', // TODO: Change this default token
   ENABLE_CONTENT_TYPE_CHECK: process.env.ENABLE_CONTENT_TYPE_CHECK
-    ? process.env.ENABLE_TYPE_CHECK === 'true'
+    ? process.env.ENABLE_CONTENT_TYPE_CHECK === 'true'
     : false,
   FFMPEG_PATH: process.env.FFMPEG_PATH || ffmpeg.path,
   MAX_VIDEO_SIZE_MB: parseInt(process.env.MAX_VIDEO_SIZE_MB || 100),
